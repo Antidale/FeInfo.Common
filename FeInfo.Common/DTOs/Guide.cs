912a9c5ce7c3a529269c6eb7c6b8331ef56b9765
antidale <antidale@gmail.com>
2024-01-05 16:26:17 -0800

    Adding Guides related dto/requests

---
+++ FeInfo.Common/DTOs/Guide.cs
@@ -0,0 +1,15 @@
+﻿using FeInfo.Common.Enums;
+
+namespace FeInfo.Common.DTOs
+{
+    public class Guide
+    {
+        public int Id { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string Tags { get; init; } = string.Empty;
+        public string Url { get; init; } = string.Empty;
+        public LinkType LinkType { get; init; }
+        public decimal Rank { get; init;} = decimal.Zero;
+    }
+}
