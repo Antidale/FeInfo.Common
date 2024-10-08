15f6d6771bc94b3824f1e799af637e214f8374ba
Antidale <antidale@tellah.life>
2024-10-08 12:15:53 -0700

    Adding extra tournament related things

---
+++ FeInfo.Common/DTOs/TournamentSummary.cs
@@ -0,0 +1,12 @@
+namespace FeInfo.Common.DTOs;
+
+public class TournamentSummary
+{
+    public int TournamentId { get; set; }
+    public string GuildName { get; set; } = string.Empty;
+    public string TournamentName { get; set; } = string.Empty;
+    public string RulesLink { get; set; } = string.Empty;
+    public DateTimeOffset RegistrationStart { get; set; }
+    public DateTimeOffset RegistrationEnd { get; set; }
+    public int EntrantCount { get; set; }
+}
