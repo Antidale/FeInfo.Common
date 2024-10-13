d655d66de55471de323caf3f2466f12b1f347d55
Antidale <antidale@tellah.life>
2024-10-12 21:19:21 -0700

    Fix typo

--- FeInfo.Common/DTOs/TournamentSummary.cs
+++ FeInfo.Common/DTOs/TournamentSummary.cs
@@ -6,7 +6,7 @@ public class TournamentSummary
     public string GuildName { get; set; } = string.Empty;
     public string TournamentName { get; set; } = string.Empty;
     public string RulesLink { get; set; } = string.Empty;
-    public string StandignsLink { get; set; } = string.Empty;
+    public string StandingsLink { get; set; } = string.Empty;
     public DateTimeOffset RegistrationStart { get; set; }
     public DateTimeOffset RegistrationEnd { get; set; }
     public int EntrantCount { get; set; }
